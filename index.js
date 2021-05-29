bab9eb6366fc5f93c4eea59ebdb1bbb026d0eaee
Ayham Al Ali <alali_ayham@yahoo.com>
2021-05-29 16:30:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const mineflayer = require('mineflayer');
-const Discord = require('discord.js');
+//const Discord = require('discord.js');
 
+const serverName = "Full" // Put your server name here
+const adMessage = "&b▁▂▃▆▉ &2INFECTED &cvs &6HUMANS &eMinigame ▍ &dPoints & Leveling System &b▉▆▃▂▁" // Put your self advertise msg here
 
 const options = {
-    username: `SkriptDev`,
-    password: ``,
+    username: 'SkriptDev', // Email OR Username
+    password: '', // Password here
     host: 'minehut.com',
     port: 25565
   };
@@ -12,7 +14,7 @@ const options = {
 var bot;
 
 function createBot(end = false) {
-    if (end)
+    if (end) // End then restart?
         bot.end();
 
     bot = mineflayer.createBot(options);
@@ -21,29 +23,40 @@ function createBot(end = false) {
 createBot();
 
 function selfAdvertise(msg = "") {
-    if (msg == "")
-        bot.chat("/ad Full &b▁▂▃▆▉ &2INFECTED &cvs &6HUMANS &eMinigame ▍ &dPoints & Leveling System &b▉▆▃▂▁");
-    else
-        bot.chat(`/ad Full ${msg}`);
+    if (msg == "") { // Default msg
+        bot.chat(`/ad ${serverName} ${adMessage}`);
+        console.log("> Self Advertised"); // Debug
+    }
+    else // Custom msg
+        bot.chat(`/ad ${serverName} ${msg}`);
+}
+
+function selfAutoAd() { // Auto Self Advertise
+    setInterval(selfAdvertise, 430000) // every 430s OR 7m and 10s (VIP)
 }
 
 bot.on("messagestr", (msg) => {
-    if (msg.match(/\[AD\] (\[.+?\] |)(\S+?): \/join (.+?) (.+)/)) {
-        console.log(msg)
+    pattern = /\[AD\] (\[.+?\]|)(?: |)(\S+?): \/join (.+?) (.+)/g
+    matches = pattern.exec(msg)
+    if (matches) {
+        console.log(`${new Date(Date.now()).toGMTString().replace(" GMT", "")}: ${msg}`) // Debug
+        //console.log(`[DEBUG] ${matches[1]} - ${matches[2]} - ${matches[3]} - ${matches[4]}`) // Debug
+        // matches[1] = Rank name if set or EMPTY
+        // matches[2] = Player name
+        // matches[3] = Server name
+        // matches[4] = AD message
+        if (matches[3] == serverName) {
+            console.log(`[MATCH] ${msg}`) // Debug
+
+        }
     }
 });
 
 bot.on("spawn", () => {
-    console.log("Spawned.");
+    console.log(`${new Date(Date.now()).toGMTString().replace(" GMT", "")}: Spawned as ${bot.username}.`);
     selfAdvertise();
-    () => {
-        setInterval(() => {
-            selfAdvertise();
-        }, 430000) // every 430s OR 7m and 10s
-
-    }
+    selfAutoAd(); // Start auto self advertiser
+        
 });
-//▁▂▃▆▉ &2INFECTED &cvs &6HUMANS &eMinigame | &dPoints & Leveling System | &c1.8-1.16▉▆▃▂▁
 
-
-bot.on('error', console.log);
+bot.on('error', console.log); // Log errors
